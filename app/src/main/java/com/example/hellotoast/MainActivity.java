c29fdb159e6a6b23d45f9b92a886861fdf4d7c51
emna-ghamlouli <emna.ghamlouli]@gmail.com>
2019-11-07 02:19:04 +0100

    tp2

---
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.hellotoast;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "";
+    private static final String LOG_TAG = "";
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+        Log.i("LOG_TAG", " La deuxième activité est ajoutée en tant que classe Java, le fichier de présentation (layout) XML est créé et le fichier AndroidManifest.xml est modifié pour déclarer une deuxième activité. ");
+        Log.i("LOG_TAG", " Le bouton Back (Précédent) ne fonctionne plus dans la deuxième activité pour renvoyer l'utilisateur à l'activité principale. ");
+        Log.i("LOG_TAG","intention explicite utilise la méthode: new Intent(Context context, Class<?> class)");
+        Log.i("LOG_TAG","on ajoute la valeur actuelle du comptage à l'intention: Comme extra d'intention (Intent)");
+        Log.i("LOG_TAG",  " Obtenez l'intention (Intent) avec laquelle l'activité a été lancée.");
+
+
+
+    }
+    public void SayHello (View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        String message = mShowCount.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+
+        startActivity(intent);
+    }
+
+
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+
+
+}
